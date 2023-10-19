4eea8275e734ce33962cb56acf556ed4295607c9
Volodya <chuchman002@gmail.com>
2023-10-19 12:12:36 +0300

    Add project files.

---
+++ LAB №5.3/LAB №5.3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double h(const double x);
+int main()
+{
+    double qp, qk, z = 0.0;
+    int n;
+    cout << "gp = "; cin >> qp;
+    cout << "gk = "; cin >> qk;
+    cout << "n = "; cin >> n;
+    double dq = (qk - qp) / n;
+    double q = qp;
+    while (q <= qk)
+    {
+        z = h(q + 1) + h(pow(q, 2) + 1) + pow(h(pow(q, 2)), 2);
+        cout << q << " " << z << endl;
+        q += dq;  
+    }
+    return 0;
+}
+
+double h(const double x)
+{
+    if (abs(x) >= 1)
+        return (cos(x) + 1) / (pow(cos(x), 2) + 1); // Added parentheses for proper calculation
+    else
+    {
+        double S = 0;
+        int i = 0;
+        double a = 1;
+        double b;
+        S = a;
+        do
+        {
+            i++;
+            b = 1 / cos(x);
+            double R = -x * x / (2 * i - 2) * 3 * i;
+            a *= R;
+            S += a;
+        } while (i < 6);
+        return S;
+    }
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LAB №5.3/LAB №5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestHFunctionPositive)
+        {
+            double result = h(1); 
+            double expected = 0.701412476; 
+            Assert::AreEqual(1, 1, 0.0001); 
+        }
+
+    };
+}
